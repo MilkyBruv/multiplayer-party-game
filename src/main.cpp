4e61213fc64d199abfcc8c84505ed644a7576f51
Max <max@maximilian.co.nz>
2023-09-22 17:05:34 +1200

    Setup raylib (actually works🤗🤗)

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,27 @@
 #include <iostream>
-#include "../lib/raylib/include/raylib.h"
+#include <raylib.h>
 
-int main(void)
+int main()
 {
+    std::cout << "running rn" << std::endl;
+
     const int screenWidth = 800;
     const int screenHeight = 600;
 
-    InitWindow(screenWidth, screenHeight, "Test Window");
+
+    InitWindow(screenWidth, screenHeight, "mising semicolon line 3");
     SetTargetFPS(60);
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
-        {
-            ClearBackground(RAYWHITE);
-            DrawText("chur blud", 20, 20, 20, BLACK);
-        }
+        ClearBackground(PINK);
+
+        DrawText("yeah buddy...", 20, 20, 40, WHITE);
+
         EndDrawing();
     }
 
     CloseWindow();
-
     return 0;
 }
