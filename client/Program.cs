b1a82f0cef57158aa6166ba946dd6363c8c99669
Max <max@maximilian.co.nz>
2023-11-27 23:16:15 +1300

    Switching to C#

---
+++ client/Program.cs
@@ -0,0 +1,9 @@
+﻿class Program
+{
+	public static void Main(string[] args)
+	{
+		Console.WriteLine("Game");
+		
+		//TODO Game.Run(); or something idk
+	}
+}
---
+++ server/Program.cs
@@ -0,0 +1,9 @@
+﻿class Program
+{
+	public static void Main(string[] args)
+	{
+		Console.WriteLine("Server");
+
+		//TODO Server.Start(); or something idk
+	}
+}
